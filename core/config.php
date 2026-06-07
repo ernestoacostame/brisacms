3e4b8254ec6dd00446deb191e63c8ad4e609585e
Ernesto Acosta <elavdeveloper@gmail.com>
2026-06-07 10:48:28 -0500

    Release BrisaCMS v2.0.0

--- core/config.php
+++ core/config.php
@@ -1,6 +1,6 @@
 <?php
 // BrisaCMS - Configuration
-define('CMS_VERSION', '1.0.6');
+define('CMS_VERSION', '2.0.0');
 define('CMS_NAME', 'BrisaCMS');
 define('ROOT_PATH', dirname(__DIR__));
 define('CONTENT_PATH', ROOT_PATH . '/content');
